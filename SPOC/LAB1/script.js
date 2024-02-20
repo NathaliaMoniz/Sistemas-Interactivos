6e1bcb8db590b336c7da74a2087e11e57b5e19e4
Nathalia Moniz Cordova <nathaliamonizcordova@gmail.com>
2024-02-20 10:39:53 +0100

    minor updates

--- SPOC/LAB1/script.js
+++ SPOC/LAB1/script.js
@@ -7,20 +7,55 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 var marker;
 
+var Icono_posiscion = L.icon({
+  iconUrl: './media/marcador-de-posicion.png',
+  iconSize: [40, 45],
+  iconAnchor: [22, 94],
+  popupAnchor: [-3, -76],
+  shadowSize: [68, 95],
+});
+
 const watchID = navigator.geolocation.watchPosition(function (position) { 
   var latitud = position.coords.latitude;
   var longitud = position.coords.longitude;
   
   mymap.setView([latitud, longitud], 20);
+  
 
   if (marker){
-    marker.setLatLng([latitud, longitud]);
+    marker.setLatLng([latitud, longitud], );
   }
   else{
-    marker = L.marker([latitud, longitud]).addTo(mymap);
+    marker = L.marker([latitud, longitud], {icon: Icono_posiscion}).addTo(mymap);
     marker.bindPopup("Estás aquí").openPopup();
   }
 });
+
+
+mymap.on('dblclick', function(e){
+  var latitud_destino = e.latlng.lat;
+  var longitud_destino = e.latlng.lng;
+
+  var confirmación = window.confirm('¿Desea agregar este punto como destino?');
+
+  if(confirmación){
+    var marker_destino = L.marker([latitud_destino, longitud_destino]).addTo(mymap);
+    var radio_destino = L.circle([latitud_destino, longitud_destino],  40).addTo(mymap);
+
+    var visible = true;
+
+    var intervalo = setInterval(function(){
+      if(visible){
+        radio_destino.setStyle({opacity: 0.7, fillOpacity: 0.3});
+      }else{
+        radio_destino.setStyle({opacity: 1, fillOpacity: 0.5});
+      }
+      visible = !visible;
+
+    }, 500);
+  }
+
+})
 mymap.on('click', function(e) {
   console.log(e);
 

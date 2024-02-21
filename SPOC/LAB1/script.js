1f1084e67f714391180a45d5e010e57c2c408131
Nathalia Moniz Cordova <nathaliamonizcordova@gmail.com>
2024-02-21 22:03:51 +0100

    latest update
    Versión lista para entregar

--- SPOC/LAB1/script.js
+++ SPOC/LAB1/script.js
@@ -7,10 +7,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 
 var marker;
-var radio_destino;
 var marker_destino;
 var destinos = [];
-
 
 var Icono_posiscion = L.icon({
   iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
@@ -23,41 +21,42 @@ var Icono_posiscion = L.icon({
       iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png'
 });
 
-pos_ini = navigator.geolocation.getCurrentPosition(function (position){
+// Se calcula la posición inicial para centrar al usuario por primera vez
+navigator.geolocation.getCurrentPosition(function (position){
   var latitud = position.coords.latitude;
   var longitud = position.coords.longitude;
   
   mymap.setView([latitud, longitud], 20);
 });
 
-function stopVibrating() {
-  navigator.vibrate(0); // Detener la vibración
-}
-
 function añadirDestino(e){
+
+  // Se crea el macador y su área
   var marker_destino = L.marker(e.latlng).addTo(mymap);
   var circle = L.circle(e.latlng, { radius: 60, opacity: 0.5 }).addTo(mymap);
 
+  // Se añaden a la lista de destinos
   destinos.push({ marker: marker_destino, circle: circle });
 
+  // Animación del círculo
   setInterval(function () {
     circle.setStyle({ opacity: circle.options.opacity === 0.5 ? 0 : 0.5 });
   }, 500);
 
+  // Asignamos el evento para eliminar los marcadores al hacer click en ellos
   marker_destino.on('click', function () {
-    setTimeout(function () {
         var confirmDelete = confirm("¿Desea eliminar este destino?");
         if (confirmDelete) {
             mymap.removeLayer(marker_destino);
             mymap.removeLayer(circle);
             destinos = destinos.filter(function (item) {
-                return item.marker_destino !== marker_destino;
-            });      
+                return item.marker !== marker_destino;
+            });   
         }
-    });
   });
 }
 
+// Asignamos para añadir marcadores al hacer doble click en ellos
 mymap.on('dblclick', function (e) {
   var addDestination = confirm("¿Desea añadir un destino?");
   if (addDestination) {
@@ -65,11 +64,11 @@ mymap.on('dblclick', function (e) {
   }
 });
 
-
-
+// Se obtiene la posición actualizada del usuario
 const watchID = navigator.geolocation.watchPosition(function (position) { 
   var latitud = position.coords.latitude;
   var longitud = position.coords.longitude;
+
   // Si ya existe el marcador de origen se actualiza su posición
   if (marker){
     marker.setLatLng([latitud, longitud]);
@@ -88,12 +87,25 @@ const watchID = navigator.geolocation.watchPosition(function (position) {
       var duracionVibracion = Math.max(0, item.circle.getRadius() - distancia);
     
       if (distancia <= item.circle.getRadius()) {
+        
+        // La vibración será más fuerte mientras más cerca esté del destino
         navigator.vibrate([duracionVibracion * 10, 50]);
-        console.log("hkjhkj")
       }
   })
   
 });
+
+// Al presionar el botón de centrar el mapa se centra en el marcador del usuario
+document.getElementById('centrar').addEventListener('click', function() {
+  navigator.geolocation.getCurrentPosition(function(position) {
+      var latitud = position.coords.latitude;
+      var longitud = position.coords.longitude;
+      
+      // Centra el mapa en la posición del usuario
+      mymap.setView([latitud, longitud], 20);
+  });
+});
+
 mymap.on('click', function(e) {
   console.log(e);
 
